238b8f35f8747fde5fda274b9e541ae932e70fb8
Tomrm96 <tomrutter96@hotmail.com>
2024-10-12 22:23:13 +0100

    Feature/Added-WIFI

---
+++ NEA/main.py
@@ -0,0 +1,17 @@
+from wifi import WIFI_CONNECTION
+
+wifi = WIFI_CONNECTION()
+
+# Connect to WIFI
+wifi.connect_to_wifi()
+
+
+# Check the Web app to look for updates to schedules
+
+
+# if update, update the current schedule dictionary. 
+
+
+
+# if there is a feed scheduled, execute the feeder
+
