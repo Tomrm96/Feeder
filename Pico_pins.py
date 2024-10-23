bb24c0e02b0a72d2afceae19d417ce7b418b42fa
Tomrm96 <tomrutter96@hotmail.com>
2024-10-23 23:36:26 +0100

    Fix/Fixed-Servo

--- Pico_pins.py
+++ Pico_pins.py
@@ -6,34 +6,31 @@ class PicoPins:
         self.led = Pin(25, Pin.OUT)
         self.servo_pin = PWM(Pin(16))
         self.servo_pin.freq(50)
-
-        self.max_duty = 7864
-        self.min_duty = 1802
-        self.half_duty = int(self.max_duty /2)
+        self.backwards_value = 6553   
+        self.forward_value = 3276   
+        self.stop_value = 4915  
 
     def blink(self):
         self.led.toggle()
 
-
-
+    def servo_forward(self, amount):
+        for i in range(0, amount):
+            self.servo_pin.duty_u16(self.forward_value)
+            time.sleep(0.6)
+            self.servo_pin.duty_u16(self.stop_value)
+            time.sleep(1)
+        self.servo_pin.deinit()
+      
+    def servo_backward(self, amount):
+        for i in range(0, amount):
+            self.servo_pin.duty_u16(self.backwards_value)
+            time.sleep(0.6)
+            self.servo_pin.duty_u16(self.stop_value)
+            time.sleep(1)
+        self.servo_pin.deinit()
+        
     def dispense_food(self):
         pass
 
-
     def weigh_food(self):
         pass
-
-
-    def servo_test(self):
-
-        self.servo_pin.duty_u16(self.min_duty)
-        time.sleep(2)
-        
-        self.servo_pin.duty_u16(self.half_duty)
-        time.sleep(2)
-
-        self.servo_pin.duty_u16(self.max_duty)
-        time.sleep(2)    
-
-        self.servo_pin.deinit()
-      
